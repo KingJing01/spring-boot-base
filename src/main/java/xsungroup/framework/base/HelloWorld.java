c03af5c927bc414ed1b54775ac30d2bd04692ea6
KingJing01 <18049808631@163.com>
2019-07-05 13:25:30 +0800

    code commit

---
+++ src/main/java/xsungroup/framework/base/HelloWorld.java
@@ -0,0 +1,14 @@
+package xsungroup.framework.base;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/")
+    public String Hello(){
+        return "Hello world";
+    }
+}

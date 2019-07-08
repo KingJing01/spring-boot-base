ae6ff411eebfbf16b9f2bcc84aae3ae362ed36ee
KingJing01 <18049808631@163.com>
2019-07-08 15:19:35 +0800

    enum resource update

--- src/main/java/xsungroup/framework/base/utils/ResultEnum.java
+++ src/main/java/xsungroup/framework/base/utils/ResultEnum.java
@@ -2,19 +2,27 @@ package xsungroup.framework.base.utils;
 
 import lombok.Getter;
 
+/**
+ * @description: 返回信息的枚举封装
+ * @author: kingJing
+ * @Date: 2019/7/8 15:17
+ */
 @Getter
 public enum ResultEnum {
-    success(0,"成功"),
-    fail(1,"操作失败"),
-    ;
-
+    SUCCESS(0, "成功"),
+    FAIL(1, "操作失败"),;
+    // 编码
     private int code;
-
+    // 消息信息
     private String message;
 
-    ResultEnum(int code,String message){
+    ResultEnum(int code, String message) {
         this.code = code;
         this.message = message;
     }
 
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }

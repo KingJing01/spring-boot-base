677b3d2a01d1a3c9fe7d35e83005700416067cae
KingJing01 <18049808631@163.com>
2019-07-09 14:39:46 +0800

    controller exception handler

--- src/main/java/xsungroup/framework/base/exception/ControllerExceptionHandler.java
+++ src/main/java/xsungroup/framework/base/exception/ControllerExceptionHandler.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import xsungroup.framework.base.utils.ExceptionEnum;
 import xsungroup.framework.base.utils.ResponseUtil;
 import xsungroup.framework.base.utils.ResultEnum;
 

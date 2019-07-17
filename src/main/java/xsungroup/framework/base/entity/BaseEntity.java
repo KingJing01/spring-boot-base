935c416dab50dc823827a25027cb8547e3466548
KingJing01 <18049808631@163.com>
2019-07-17 11:22:18 +0800

    common method update

--- src/main/java/xsungroup/framework/base/entity/BaseEntity.java
+++ src/main/java/xsungroup/framework/base/entity/BaseEntity.java
@@ -5,19 +5,19 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
 
 import java.io.Serializable;
 import java.util.Date;
 
 /**
-* @Description: 基础Entity封装.
-* @Author: kingJing
-* @Date: 2019/7/5 13:53
-**/
-@Data
-public abstract class BaseEntity implements Serializable{
+ * @Description: 基础Entity封装.
+ * @Author: kingJing
+ * @Date: 2019/7/5 13:53
+ **/
+public abstract class BaseEntity implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @TableId(type = IdType.UUID)
@@ -36,9 +36,10 @@ public abstract class BaseEntity implements Serializable{
     @TableField(fill = FieldFill.INSERT_UPDATE)
     @ApiModelProperty(hidden = true)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+
     private Date updateTime;
 
-    @TableField(fill=FieldFill.INSERT_UPDATE)
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     @ApiModelProperty(hidden = true)
     private String updateUser;
 
@@ -57,4 +58,84 @@ public abstract class BaseEntity implements Serializable{
 
     @TableField(exist = false)
     private String columns;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public int getIsAble() {
+        return isAble;
+    }
+
+    public void setIsAble(int isAble) {
+        this.isAble = isAble;
+    }
+
+    @JsonIgnore
+    public long getPageSize() {
+        return pageSize;
+    }
+
+    @JsonProperty
+    public void setPageSize(long pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    @JsonIgnore
+    public long getPageNum() {
+        return pageNum;
+    }
+
+    @JsonProperty
+    public void setPageNum(long pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    @JsonIgnore
+    public String getColumns() {
+        return columns;
+    }
+
+    @JsonProperty
+    public void setColumns(String columns) {
+        this.columns = columns;
+    }
+
+
 }
